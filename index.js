5a43959e8470189adb5934e26e40b666e9139bdb
ElyssaJyu <sunelyssa@microsoft.com>
2023-02-15 17:04:26 +0800

    email format

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const { includes } = require("resium");
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-
+const nodemailer = require('nodemailer');
 
 const first_filter_comb = ["safty", "security", "concern"];
 const second_filter_comb = ["data", "password", "profile"];
 const filter_kw = "privacy";
 const issue = github.context.payload.issue;
+const email_password = core.getInput('email_password');
 
-console.log(issue)
+core.debug(issue)
 
 main()
 
 function main(){
     var need_attention = false;
     try{
-        if (issue.title.includes(filter_kw) || issue.body.includes(filter_kw))
+        if (issue?.title?.includes(filter_kw) || issue?.body?.includes(filter_kw))
         {
             setOutput();
             need_attention = true;
@@ -24,8 +24,8 @@ function main(){
         else{
             for (let item1 of first_filter_comb) {
                 for (let item2 of second_filter_comb) {
-                    if ((issue.title.includes(item1) && issue.title.includes(item2)) || 
-                    (issue.body.includes(item1) && issue.body.includes(item2)))
+                    if ((issue?.title?.includes(item1) && issue?.title?.includes(item2)) || 
+                    (issue?.body?.includes(item1) && issue?.body?.includes(item2)))
                     {
                         setOutput();
                         need_attention = true;
@@ -38,12 +38,11 @@ function main(){
             };
         }
         if (!need_attention){
-            console.log("No Need Attention")
             core.setOutput("need_attention", 'false');
         }
     }
     catch(err){
-        console.log("Filter Fails" + err.message)
+        core.error(`Error ${err}`);
     }
 }
 
@@ -51,12 +50,126 @@ function setOutput() {
     var data ={
         "title":"privacy",
         "issueName":issue.title,
-        "issueLink":issue.url,
+        "issueLink":issue.html_url,
         "issueNumber":issue.number,
-        "issueCreateTime":issue.time
+        "issueCreateTime":issue.created_at
     }
     var jsonData = JSON.stringify(data);
     core.setOutput("need_attention", 'true');
-    console.log("Need Attention")
     core.setOutput("issue_info", jsonData);
+    core.warning("Alarm: new high priority issue need to look into!\n" + issue.html_url)
+    sendMail()
+}
+
+function sendMail(){
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        auth: {
+        user: 'autodigectbot@outlook.com', 
+        pass: email_password
+        }
+    });
+
+    const emailContent = `
+    <html>
+        <body style="background-color:grey">
+            <table align="center" border="0" cellpadding="0" cellspacing="0"
+                width="550" bgcolor="white" style="border:2px solid black">
+                <tbody>
+                    <tr>
+                        <td align="center">
+                            <table align="center" border="0" cellpadding="0" 
+                                cellspacing="0" class="col-550" width="550">
+                                <tbody>
+                                    <tr>
+                                        <td align="center" style="background-color: #4cb96b;
+                                                height: 50px;">
+        
+                                            <a href="#" style="text-decoration: none;">
+                                                <p style="color:white;
+                                                        font-weight:bold;">
+                                                    GitHub Auto-Digest Bot
+                                                </p>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr style="height: 300px;">
+                        <td align="center" style="border: none;
+                                border-bottom: 2px solid #4cb96b; 
+                                padding-right: 20px;padding-left:20px">
+        
+                            <p style="font-weight: bolder;font-size: 42px;
+                                    letter-spacing: 0.025em;
+                                    color:black;">
+                                    Alarm!
+                                <br> New high priority issue need to look into!
+                            </p>
+                        </td>
+                    </tr>
+        
+                    <tr style="display: inline-block;">
+                        <td style="height: 150px;
+                                padding: 20px;
+                                border: none; 
+                                border-bottom: 2px solid #361B0E;
+                                background-color: white;">
+                            
+                            <h2 style="text-align: left; align-items: center;">
+                                issue title:${issue.title}
+                            </h2>
+                            <p class="data" 
+                                style="text-align: justify-all;
+                                align-items: center; 
+                                font-size: 15px;
+                                padding-bottom: 12px;">
+                                issue number:${issue.number}
+                            </p>
+                            <p class="data" 
+                                style="text-align: justify-all;
+                                align-items: center; 
+                                font-size: 15px;
+                                padding-bottom: 12px;">
+                                issue create time:${issue.created_at}
+                            </p>
+                            <p>
+                                <a href="${issue.html_url}"
+                                style="text-decoration: none; 
+                                        color:black; 
+                                        border: 2px solid #4cb96b; 
+                                        padding: 10px 30px;
+                                        font-weight: bold;"> 
+                                View Issue 
+                                </a>
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </body>
+    </html>
+    `
+    
+    let mailOptions = {
+        from: 'autodigectbot@outlook.com', 
+        to: 'sunelyssa@microsoft.com', 
+        subject: 'Alarm: new high priority issue need to look into!',
+        //text: `issue title:${issue.title}` + `\n` +`issue link:${issue.html_url}` + `\n` + `issue number:${issue.number}` + `\n` +`issue create time:${issue.created_at}`
+        text: emailContent
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        }
+    });
+  
 }
