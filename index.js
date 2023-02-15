b9b26838a9da3b4b36f0f05e784dc2d74dfeb40a
Xu Zhang <xuzhang4@microsoft.com>
2023-02-15 11:20:10 +0800

    need github

--- index.js
+++ index.js
@@ -7,30 +7,13 @@ const first_filter_comb = ["safety", "security", "concern"];
 const second_filter_comb = ["data", "password", "profile"];
 const filter_kw = "privacy";
 
-
-const env = process.env;
-console.log(env)
-const env_title = env.title
-const env_body = env.body
-const env_time = env.created_at
-const env_url = env.html_url
-const env_number = env.number
-
-console.log("env:" + env.env_issue)
-console.log(env.string_issue)
-
-console.log("github:" + github)
-console.log(github.context)
-console.log(github.context.payload)
-console.log(JSON.stringify(github.context.payload, undefined, 2))
-
-
+const issue = github.context.payload.issue;
 
 main()
 
 function main(){
     var need_attention = false;
-    if (env_title.includes(filter_kw) || env_body.includes(filter_kw))
+    if (issue.title.includes(filter_kw) || issue.body.includes(filter_kw))
     {
         setOutput();
         need_attention = true;
@@ -38,8 +21,8 @@ function main(){
     else{
         for (let item1 of first_filter_comb) {
             for (let item2 of second_filter_comb) {
-                if ((env_title.includes(item1) && env_title.includes(item2)) || 
-                (env_body.includes(item1) && env_body.includes(item2)))
+                if ((issue.title.includes(item1) && issue.title.includes(item2)) || 
+                (issue.body.includes(item1) && issue.body.includes(item2)))
                 {
                     setOutput();
                     need_attention = true;
@@ -60,10 +43,10 @@ function main(){
 function setOutput() {
     var data ={
         "title":"privacy",
-        "issueName":env_title,
-        "issueLink":env_url,
-        "issueNumber":env_number,
-        "issueCreateTime":env_time
+        "issueName":issue.title,
+        "issueLink":issue.url,
+        "issueNumber":issue.number,
+        "issueCreateTime":issue.time
     }
     var jsonData = JSON.stringify(data);
     core.setOutput("need_attention", 'true');
